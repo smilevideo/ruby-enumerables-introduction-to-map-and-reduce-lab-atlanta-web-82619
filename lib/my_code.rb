4e6b0d3bb25895494cdbf8739619daa2bde5464c
andrew kim <andrewk1216@gmail.com>
2019-08-20 23:35:54 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,54 @@
 # My Code here....
+def map_to_negativize(ary)
+  new = []
+  
+  ary.each_with_index {|val, index| new[index] = -val} 
+  return new
+end
+
+def map_to_no_change(ary)
+  return ary
+end
+
+def map_to_double(ary)
+  new = []
+  ary.each_with_index {|val,index| new[index] = 2 * val}
+  return new
+end
+
+def map_to_square(ary)
+  new = []
+  ary.each_with_index {|val,index| new[index] = val ** 2}
+  return new
+end
+
+def reduce_to_total(ary, start = 0)
+  total = start
+  
+  ary.each {|val| total += val}
+  return total
+end
+
+def reduce_to_all_true (ary)
+  truth = true
+  
+  ary.each { |val|
+    if not val
+      truth = false      
+    end
+  }
+  
+  return truth
+end
+
+def reduce_to_any_true(ary)
+  truth = false
+  
+  ary.each {|val|
+    if val
+      truth = true
+    end
+  }
+  
+  return truth
+end
